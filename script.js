3638555be0ddd0f3e7b7ed03d614fb1912affbae
dedektegarapriyandi <dedektegarapriyandi@gmail.com>
2021-09-22 21:34:05 +0700

    create getTodo function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector(".todo-input");
 const submit = document.querySelector(".submit-btn");
+const todoList = document.querySelector(".todo-list")
 
 addTodo = (e) => {
     e.preventDefault();
@@ -18,7 +19,39 @@ addTodo = (e) => {
         todos.push(item);
         localStorage.setItem("todos", JSON.stringify(todos));
         input.value = "";
+
+        document.location.reload();
     }
 }
 
+getTodo = () => {
+    // local storage check
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    }else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.forEach((todo) => {
+        // div
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo-item");
+
+        // item name
+        const itemName = document.createElement("li");
+        itemName.classList.add("item-name");
+        itemName.innerText = todo;
+        todoDiv.appendChild(itemName);
+
+        // button 
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("delete-btn");
+        deleteBtn.innerText = "delete";
+
+        // merged list to todo list
+        todoList.appendChild(todoDiv);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", getTodo);
 submit.addEventListener("click", addTodo);
